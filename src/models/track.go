1050139f860a2182f9f1448ff7b4516f01d42084
RubenPari <rubenpari.developer@gmail.com>
2022-10-30 21:36:53 +0100

    edited track model

--- src/models/track.go
+++ src/models/track.go
@@ -4,11 +4,13 @@ import (
 	"github.com/zmb3/spotify/v2"
 )
 
+// Track NOTE: featuring attribute is a string concatenation
+// of all name of the artist featuring separated by ', '
 type Track struct {
 	Id        spotify.ID  `json:"id"`
 	Name      string      `json:"name"`
 	Uri       spotify.URI `json:"uri"`
 	Album     string      `json:"album"`
 	Artist    string      `json:"artist"`
-	Featuring []string    `json:"featuring"`
+	Featuring string      `json:"featuring"`
 }
