dd9b33e928826d08fde97586d260d5d784d87125
RubenPari <rubenpari.developer@gmail.com>
2022-10-31 22:04:20 +0100

    added functions utilities

---
+++ src/modules/client/client.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"encoding/json"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+// GetNameArtistById
+// call to endpoint to get name of artist by id
+func GetNameArtistById(id string) (string, error) {
+	_ = godotenv.Load()
+	port := os.Getenv("PORT")
+
+	// get name of artist by id with endpoint
+	responseName, errGetName := http.Get("http://localhost:" + port + "/utils/artist/get-name/" + id)
+	if errGetName != nil {
+		log.Default().Println("Error getting name of artist")
+		log.Default().Println(errGetName)
+		return "", errGetName
+	}
+
+	// extract name of artist from response of type json
+	type Response struct {
+		Status string `json:"status"`
+		Name   string `json:"name"`
+	}
+
+	var response Response
+	_ = json.NewDecoder(responseName.Body).Decode(&response)
+
+	return response.Name, nil
+}
